f3ec228f61ef6b61d64240845ef7ace8d841b1f8
Indre Bakeer <indre.bakeer@gmail.com>
2019-02-13 21:55:37 +0000

    Automatically backed up by Learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -8,6 +8,15 @@ class ChattyStudent < Student
   def raise_hand
     super
     puts "Pick me!"
+    puts "Pick me!"
+    puts "Pick me!"
+    puts "Pick me!"
+    puts "Pick me!"
+    puts "Pick me!"
+    puts "Pick me!"
+    puts "Pick me!"
+    puts "Pick me!"
+
   end
 
 end
